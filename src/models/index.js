58a780786696f85168e28020e2ad3428ba023a49
Petro Zubar <pmzubar@gmail.com>
2019-10-06 23:02:21 +0300

    adds model file

---
+++ src/models/index.js
@@ -0,0 +1,18 @@
+import db from './db'
+
+const getPublications = () => {
+  const citiesRef = db.collection("articles").where("categories", "array-contains", "Володимир Зеленський");
+  citiesRef.get()
+    .then(function (querySnapshot) {
+      querySnapshot.forEach(function (doc) {
+        debugger;
+        // doc.data() is never undefined for query doc snapshots
+        console.log(doc.id, " => ", doc.data());
+      });
+    })
+    .catch(function (error) {
+      console.log("Error getting documents: ", error);
+    });
+
+
+};
