b47deb2ecd61adda1bcfacf16303d48fe7e3ccc7
pzubar <pmzubar@gmail.com>
2019-11-14 09:15:38 +0200

    improves homepage

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,14 +1,13 @@
-import React, {Component, useState, useEffect} from 'react';
+import React, {Component, useState, useEffect, useMemo} from 'react';
 import {Card, CardBody, CardColumns, CardHeader, Col, Row} from "reactstrap";
 import db from '../../models/db.js'
-import { Link } from 'react-router-dom';
+import {Link} from 'react-router-dom';
 import {database} from "../../models/db";
 import test from './test'
 import Highcharts from 'highcharts';
 import HighchartsReact from "highcharts-react-official";
 import {Bar} from "react-chartjs-2";
 import {CustomTooltips} from "@coreui/coreui-plugin-chartjs-custom-tooltips";
-
 
 const bar = {
   labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
@@ -33,11 +32,57 @@ const options = {
   maintainAspectRatio: false
 }
 
-const Home = (props) => {
-  const [articlesNumber, setArticlesNumber] = useState(0);
+const Home = () => {
   const [categoriesNumber, setCategoriesNumber] = useState(0);
   const [articlesList, setArticlesList] = useState([]);
-
+  const [categoriesList, setCategories] = useState([]);
+  const articlesNumber = useMemo(() => articlesList.length, [articlesList]);
+  const persons = useMemo(() =>
+      Object.entries(categoriesList).filter(([, {count, type}]) => count && type === "Персони"),
+    [categoriesList]);
+  const events = useMemo(() =>
+      Object.entries(categoriesList).filter(([, {count, type}]) => count && type === "Події"),
+    [categoriesList]);
+  const periodsOptions = {
+    chart: {
+      plotBackgroundColor: null,
+      plotBorderWidth: null,
+      plotShadow: false,
+      type: 'pie'
+    },
+    title: {
+      text: 'Періодизація мемів'
+    },
+    tooltip: {
+      pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+    },
+    plotOptions: {
+      pie: {
+        allowPointSelect: true,
+        cursor: 'pointer',
+        dataLabels: {
+          enabled: true,
+          format: '<b>{point.name}</b>: {point.percentage:.1f} %'
+        }
+      }
+    },
+    series: [{
+      name: 'Brands',
+      colorByPoint: true,
+      data: [{
+        name: 'Десяті',
+        y: 61.41,
+        sliced: true,
+        selected: true
+      }, {
+        name: 'Нульові',
+        y: 11.84
+      }, {
+        name: "Девя'ності",
+        y: 10.85
+      }]
+    }]
+  };
   useEffect(() => {
 
     // database.ref('/').once('value').then(function(snapshot) {
@@ -46,10 +91,10 @@ const Home = (props) => {
     // });
 
     const {articles, categories} = test;
-    debugger;
+    // debugger;
     setArticlesList(Object.keys(articles.titles));
     setCategoriesNumber(Object.keys(categories).length)
-
+    setCategories(categories)
     // const categories = {};
     // const result = [];
     // const articles = {};
@@ -88,91 +133,71 @@ const Home = (props) => {
   return (
     <div className="animated fadeIn">
       <Row style={{marginTop: 20}}>
-        <Col xs="12" sm="6" lg="6">
+        <Col xs="12">
+          <p><b>Зараз на сайті:</b></p>
+        </Col>
+        <Col xs="12" sm="3" lg="3">
           <Card className="text-white bg-info">
             <CardBody>
-              <div className="text-value">{articlesList.length}</div>
-              <div>Статей</div>
+              <div className="text-value">{articlesNumber}</div>
+              <div>Статей про меми</div>
             </CardBody>
           </Card>
         </Col>
-        <Col xs="12" sm="6" lg="6">
+        <Col xs="12" sm="3" lg="3">
           <Card className="text-white bg-primary">
             <CardBody>
               <div className="text-value">{categoriesNumber}</div>
-              <div>Категорій</div>
+              <div>Категорій статей</div>
+            </CardBody>
+          </Card>
+        </Col>
+        <Col xs="12" sm="3" lg="3">
+          <Card className="text-white bg-warning">
+            <CardBody>
+              <div className="text-value">{persons.length}</div>
+              <div>Меметичних персон</div>
+            </CardBody>
+          </Card>
+        </Col>
+        <Col xs="12" sm="3" lg="3">
+          <Card className="text-white bg-danger">
+            <CardBody>
+              <div className="text-value">{events.length}</div>
+              <div>Мемних подій</div>
             </CardBody>
           </Card>
         </Col>
       </Row>
       <Row>
         <div>
-        {/*{articlesList.map(article => <div><Link to={`/article/${article}`}>{article}</Link></div>)}*/}
+          {/*{articlesList.map(article => <div><Link to={`/article/${article}`}>{article}</Link></div>)}*/}
         </div>
       </Row>
       <Row>
         <Col xs="12" sm="6" lg="6">
-        <Card>
-          <CardHeader>
-            Bar Chart
-            <div className="card-header-actions">
-              <a href="http://www.chartjs.org" className="card-header-action">
-                <small className="text-muted">docs</small>
-              </a>
-            </div>
-          </CardHeader>
-          <CardBody>
-            <div className="chart-wrapper">
-              <Bar data={bar} options={options} />
-            </div>
-          </CardBody>
-        </Card>
+          <Card>
+            <CardHeader>
+              Bar Chart
+              <div className="card-header-actions">
+                <a href="http://www.chartjs.org" className="card-header-action">
+                  <small className="text-muted">docs</small>
+                </a>
+              </div>
+            </CardHeader>
+            <CardBody>
+              <div className="chart-wrapper">
+                <Bar data={bar} options={options}/>
+              </div>
+            </CardBody>
+          </Card>
         </Col>
         <Col xs="12" sm="6" lg="6">
 
-      <HighchartsReact
-        highcharts={Highcharts}
-        options={{
-          chart: {
-            plotBackgroundColor: null,
-            plotBorderWidth: null,
-            plotShadow: false,
-            type: 'pie'
-          },
-          title: {
-            text: 'Періодизація мемів'
-          },
-          tooltip: {
-            pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
-          },
-          plotOptions: {
-            pie: {
-              allowPointSelect: true,
-              cursor: 'pointer',
-              dataLabels: {
-                enabled: true,
-                format: '<b>{point.name}</b>: {point.percentage:.1f} %'
-              }
-            }
-          },
-          series: [{
-            name: 'Brands',
-            colorByPoint: true,
-            data: [{
-              name: 'Десяті',
-              y: 61.41,
-              sliced: true,
-              selected: true
-            }, {
-              name: 'Нульові',
-              y: 11.84
-            }, {
-              name: "Девя'ності",
-              y: 10.85
-            }]
-          }]
-        }}
-      />
+          <HighchartsReact
+            highcharts={Highcharts}
+            options={periodsOptions}
+          />
         </Col>
       </Row>
     </div>
